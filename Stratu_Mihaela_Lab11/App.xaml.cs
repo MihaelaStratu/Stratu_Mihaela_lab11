b090acb5f873023c476fca5b1946094f1cabde7f
MIHAELA STRATU <mihaela.stratu@stud.ubbcluj.ro>
2023-12-21 11:57:32 +0200

    Add project files.

---
+++ Stratu_Mihaela_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Stratu_Mihaela_Lab11.Data;
+
+namespace Stratu_Mihaela_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
